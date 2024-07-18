15fb6e28e3272cca7df602624ffced81a1501897
Guille0418 <165737970+Guille0418@users.noreply.github.com>
2024-07-17 19:34:52 -0500

    Add files via upload

---
+++ MIA/js/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('revealMessage').addEventListener('click', function() {
+    var message = document.getElementById('hiddenMessage');
+    if (message.classList.contains('hidden')) {
+        message.classList.remove('hidden');
+    } else {
+        message.classList.add('hidden');
+    }
+});
